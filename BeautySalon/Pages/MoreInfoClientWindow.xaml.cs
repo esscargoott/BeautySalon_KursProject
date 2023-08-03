8f0bb115a149397c17ff5bc8575c9d9b8b25e767
Mazyrina Varvara <varvaramaz@gmail.com>
2023-08-03 14:22:02 +0300

    Начата работка над ABC-анализом

---
+++ BeautySalon/Pages/MoreInfoClientWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BeautySalon.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MoreInfoClientWindow.xaml
+    /// </summary>
+    public partial class MoreInfoClientWindow : Window
+    {
+        private Client _currentClient = new Client();
+        public MoreInfoClientWindow(Client selectedClient)
+        {
+            InitializeComponent();
+            if (selectedClient != null)
+            { _currentClient = selectedClient; }
+            DataContext = _currentClient;
+        }
+    }
+}
